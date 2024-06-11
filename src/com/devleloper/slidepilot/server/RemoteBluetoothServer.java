259f6f1a47f1e107423002e34eaef1eef1be7b7d
DevletBoltaev <devletb@icloud.com>
2024-06-11 23:13:27 +0500

    first commit

---
+++ src/com/devleloper/slidepilot/server/RemoteBluetoothServer.java
@@ -0,0 +1,9 @@
+package com.devleloper.slidepilot.server;
+
+public class RemoteBluetoothServer{
+
+    public static void main(String[] args) {
+        Thread waitThread = new Thread(new WaitThread());
+        waitThread.start();
+    }
+}
