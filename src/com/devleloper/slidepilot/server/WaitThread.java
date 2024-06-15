dfcd7a699a0b960fd9ca371c326b7a219745929c
DevletBoltaev <devletb@icloud.com>
2024-06-15 13:07:45 +0500

    Fixed a bug with stopping server

--- src/com/devleloper/slidepilot/server/WaitThread.java
+++ src/com/devleloper/slidepilot/server/WaitThread.java
@@ -11,69 +11,67 @@ import javax.microedition.io.StreamConnectionNotifier;
 
 public class WaitThread implements Runnable {
 
-	private boolean threadStop = false;
+    private boolean threadStop = false;
+    ProcessConnectionThread processConnectionThread;
 
-	ProcessConnectionThread processConnectionThread;
+    /** Constructor */
+    public WaitThread() {
+    }
 
-	/** Constructor */
-	public WaitThread() {
-	}
+    void setThreadStopper(boolean threadStop) {
+        this.threadStop = threadStop;
+        if (processConnectionThread != null) {
+            processConnectionThread.setThreadStopper(threadStop);
+        }
+        if (threadStop) {
+            try {
+                if (notifier != null) {
+                    notifier.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	void setThreadStopper(boolean threadStop) {
-		this.threadStop = threadStop;
-		if (processConnectionThread != null)
-			processConnectionThread.setThreadStopper(threadStop);
-		if (threadStop) {
-			try {
-				notifier.close();
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    @Override
+    public void run() {
+        waitForConnection();
+    }
 
-	@Override
-	public void run() {
-		waitForConnection();
-	}
+    private StreamConnection connection = null;
+    private StreamConnectionNotifier notifier;
+    LocalDevice local = null;
 
-	private StreamConnection connection = null;
-	private StreamConnectionNotifier notifier;
-	LocalDevice local = null;
+    /** Waiting for connection from devices */
+    private void waitForConnection() {
+        try {
+            local = LocalDevice.getLocalDevice();
+            if (local.getDiscoverable() != DiscoveryAgent.GIAC) {
+                local.setDiscoverable(DiscoveryAgent.GIAC);
+            }
 
-	/** Waiting for connection from devices */
-	private void waitForConnection() {
-		// retrieve the local Bluetooth device object
+            UUID uuid = new UUID(80087355); 
+            String url = "btspp://localhost:" + uuid.toString() + ";name=RemoteBluetooth";
+            notifier = (StreamConnectionNotifier) Connector.open(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
 
-		// setup the server to listen for connection
-		try {
-			local = LocalDevice.getLocalDevice();
-			if (local.getDiscoverable() != DiscoveryAgent.GIAC) {
-				local.setDiscoverable(DiscoveryAgent.GIAC);
-			}
-
-			UUID uuid = new UUID(80087355); // "04c6093b-0000-1000-8000-00805f9b34fb"
-			String url = "btspp://localhost:" + uuid.toString() + ";name=RemoteBluetooth";
-			notifier = (StreamConnectionNotifier) Connector.open(url);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		}
-		// waiting for connection
-//        while(true) {
-		try {
-			System.out.println("waiting for connection...");
-			connection = notifier.acceptAndOpen();
-			processConnectionThread = new ProcessConnectionThread(connection);
-			Thread processThread = new Thread(processConnectionThread);
-			processThread.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		}
-//        }
-	}
+        try {
+            System.out.println("waiting for connection...");
+            connection = notifier.acceptAndOpen();
+            processConnectionThread = new ProcessConnectionThread(connection);
+            Thread processThread = new Thread(processConnectionThread);
+            processThread.start();
+        } catch (Exception e) {
+            if (!threadStop) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
