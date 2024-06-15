6c0cd81ce6c8a50c7358a111265b41141c403971
DevletBoltaev <devletb@icloud.com>
2024-06-16 01:09:23 +0500

    Fixed cursor movement

--- src/com/devleloper/slidepilot/server/WaitThread.java
+++ src/com/devleloper/slidepilot/server/WaitThread.java
@@ -1,7 +1,6 @@
 package com.devleloper.slidepilot.server;
 
 import java.io.IOException;
-
 import javax.bluetooth.DiscoveryAgent;
 import javax.bluetooth.LocalDevice;
 import javax.bluetooth.UUID;
@@ -11,69 +10,54 @@ import javax.microedition.io.StreamConnectionNotifier;
 
 public class WaitThread implements Runnable {
 
-	private boolean threadStop = false;
+    private boolean threadStop = false;
+    private StreamConnection connection = null;
+    private StreamConnectionNotifier notifier;
+    private ProcessConnectionThread processConnectionThread;
 
-	ProcessConnectionThread processConnectionThread;
+    void setThreadStopper(boolean threadStop) {
+        this.threadStop = threadStop;
+        if (processConnectionThread != null) {
+            processConnectionThread.setThreadStopper(threadStop);
+        }
+        if (threadStop) {
+            try {
+                if (notifier != null) {
+                    notifier.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	/** Constructor */
-	public WaitThread() {
-	}
+    @Override
+    public void run() {
+        waitForConnection();
+    }
 
-	void setThreadStopper(boolean threadStop) {
-		this.threadStop = threadStop;
-		if (processConnectionThread != null)
-			processConnectionThread.setThreadStopper(threadStop);
-		if (threadStop) {
-			try {
-				notifier.close();
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    private void waitForConnection() {
+        try {
+            LocalDevice local = LocalDevice.getLocalDevice();
+            if (local.getDiscoverable() != DiscoveryAgent.GIAC) {
+                local.setDiscoverable(DiscoveryAgent.GIAC);
+            }
 
-	@Override
-	public void run() {
-		waitForConnection();
-	}
+            UUID uuid = new UUID(80087355); // "04c6093b-0000-1000-8000-00805f9b34fb"
+            String url = "btspp://localhost:" + uuid.toString() + ";name=RemoteBluetooth";
+            notifier = (StreamConnectionNotifier) Connector.open(url);
 
-	private StreamConnection connection = null;
-	private StreamConnectionNotifier notifier;
-	LocalDevice local = null;
+            System.out.println("waiting for connection...");
+            connection = notifier.acceptAndOpen();
+            processConnectionThread = new ProcessConnectionThread(connection);
+            Thread processThread = new Thread(processConnectionThread);
+            processThread.start();
 
-	/** Waiting for connection from devices */
-	private void waitForConnection() {
-		// retrieve the local Bluetooth device object
-
-		// setup the server to listen for connection
-		try {
-			local = LocalDevice.getLocalDevice();
-			if (local.getDiscoverable() != DiscoveryAgent.GIAC) {
-				local.setDiscoverable(DiscoveryAgent.GIAC);
-			}
-
-			UUID uuid = new UUID(80087355); // "04c6093b-0000-1000-8000-00805f9b34fb"
-			String url = "btspp://localhost:" + uuid.toString() + ";name=RemoteBluetooth";
-			notifier = (StreamConnectionNotifier) Connector.open(url);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		}
-		// waiting for connection
-//        while(true) {
-		try {
-			System.out.println("waiting for connection...");
-			connection = notifier.acceptAndOpen();
-			processConnectionThread = new ProcessConnectionThread(connection);
-			Thread processThread = new Thread(processConnectionThread);
-			processThread.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		}
-//        }
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
